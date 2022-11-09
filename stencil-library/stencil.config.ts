be72f542bdec3d8d540f49e394b05957c9967b0f
João Matos <CTW02505@criticaltechworks.com>
2022-11-09 03:13:19 +0000

    install @stencil/angular-output-target with changes at stencil-config-ts

--- stencil-library/stencil.config.ts
+++ stencil-library/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target'; 
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -17,5 +18,10 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library', // changed this path to the name of the package.json inside the folder "stencil-library"
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',   
+    }),
   ],
 };
